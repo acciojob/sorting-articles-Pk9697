e02abe013534291f833953ae71a16148de8e986d
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-19 13:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const band=document.getElementById('band')
+
+const listItems=bands.sort((a,b)=>{
+	a=a.toLowerCase()
+	b=b.toLowerCase()
+	const firstWordA=a.split(' ')[0]
+	const remWordA=a.split(' ').slice(1).join(' ');
+	const firstWordB=b.split(' ')[0]
+	const remWordB=b.split(' ').slice(1).join(' ');
+
+
+	if(firstWordA==='a' || firstWordA==='an' || firstWordA==='the'){
+		a=remWordA;
+	}
+	if(firstWordB==='a' || firstWordB==='an' || firstWordB==='the'){
+		b=remWordB;
+	}	
+
+	return a<b?-1:1;
+}).map((item)=>`<li>${item}</li>`).join(' ')
+
+console.log(listItems)
+
+band.innerHTML=`
+	<ul>
+		${listItems}
+	</ul>
+`
